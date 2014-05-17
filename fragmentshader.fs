304571360ddeb20ed3176b6f1add7d440ce7f90b
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2014-05-17 14:17:28 +0200

    Add specular reflection to FS

--- fragmentshader.fs
+++ fragmentshader.fs
@@ -32,7 +32,12 @@ void main () {
   vec3 Id = Ld * Kd * dot_prod; // final diffuse intensity
   
   // specular intensity
-  vec3 Is = vec3 (0.0, 0.0, 0.0); // replace me later
+  vec3 reflection_eye = reflect (-direction_to_light_eye, normal_eye);
+  vec3 surface_to_viewer_eye = normalize (-position_eye);
+  float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
+  dot_prod_specular = max (dot_prod_specular, 0.0);
+  float specular_factor = pow (dot_prod_specular, specular_exponent);
+  vec3 Is = Ls * Ks * specular_factor; // final specular intensity
   
   // final colour
   fragment_colour = vec4 (Is + Id + Ia, 1.0);

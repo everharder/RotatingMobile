9c86233a6d3f907de63c0be5d2c43cd4ac26e168
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2014-05-17 15:50:30 +0200

    Temporary commit

--- fragmentshader.fs
+++ fragmentshader.fs
@@ -1,5 +1,6 @@
 #version 330
 
+in vec4 vertex_eye;
 in vec3 position_eye, normal_eye;
 
 uniform mat4 ViewMatrix;
@@ -16,7 +17,7 @@ vec3 Kd = vec3 (1.0, 0.5, 0.0); // orange diffuse surface reflectance
 vec3 Ka = vec3 (1.0, 1.0, 1.0); // fully reflect ambient light
 float specular_exponent = 100.0; // specular 'power'
 
-out vec4 fragment_colour; // final colour of surface
+smooth out vec4 fragment_colour; // final colour of surface
 
 void main () {
   // ambient intensity
@@ -24,16 +25,18 @@ void main () {
 
   // diffuse intensity
   // raise light position to eye space
-  vec3 light_position_eye = vec3 (ViewMatrix * vec4 (light_position_world, 1.0));
-  vec3 direction_to_light_eye = normalize (light_position_eye - position_eye);
-  float dot_prod = dot (direction_to_light_eye, normal_eye);
+  //vec3 light_position_eye = vec3 (ViewMatrix * vec4 (light_position_world, 1.0));
+  vec3 direction_to_light_eye = normalize (light_position_world - vec3(vertex_eye));
+  vec3 direction_to_normal = normalize(normal_eye);
+  float dot_prod = dot (direction_to_light_eye, direction_to_normal);
   dot_prod = max (dot_prod, 0.0);
   vec3 Id = Ld * Kd * dot_prod; // final diffuse intensity
   
   // specular intensity
-  vec3 reflection_eye = reflect (-direction_to_light_eye, normal_eye);
-  vec3 surface_to_viewer_eye = normalize (-position_eye);
-  float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
+  vec3 direction_reflection = reflect (-direction_to_light_eye, direction_to_normal);
+  //vec3 surface_to_viewer_eye = normalize (-position_eye);
+  vec3 direction_position_eye = normalize (position_eye);
+  float dot_prod_specular = dot (direction_reflection, direction_position_eye);
   dot_prod_specular = max (dot_prod_specular, 0.0);
   float specular_factor = pow (dot_prod_specular, specular_exponent);
   vec3 Is = Ls * Ks * specular_factor; // final specular intensity
--- vertexshader.vs
+++ vertexshader.vs
@@ -7,12 +7,14 @@ uniform mat4 ModelMatrix;
 layout (location = 0) in vec3 Position;
 layout (location = 1) in vec3 Normal;
 
+out vec4 vertex_eye;
 out vec3 position_eye, normal_eye;
 
 void main()
 {
-  position_eye = vec3 (ViewMatrix*ModelMatrix*vec4(Position, 1.0));
+  vertex_eye = ModelMatrix * vec4(Position, 1.0);
+  position_eye = vec3 (inverse(ViewMatrix) * vec4(0.0, 0.0, 0.0, 1.0) - vertex_eye);
   //normal_eye = vec3 (ViewMatrix*ModelMatrix*vec4(Normal, 0.0));
   normal_eye = vec3 (ModelMatrix*vec4(Normal, 0.0));
-  gl_Position = ProjectionMatrix * vec4(position_eye, 1.0);
+  gl_Position = ProjectionMatrix * ViewMatrix * vertex_eye;
 }

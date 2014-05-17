c0ffe6b4efbbf0b445077ace44ebf77036c8a035
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2014-05-17 19:47:45 +0200

    Add Phong Shading

--- fragmentshader_phong.fs
+++ fragmentshader_phong.fs
@@ -1,21 +1,34 @@
 #version 330
 
-smooth in vec3 normalPosition;
-smooth in vec3 viewPosition;
+in vec3 normalDirection;
+in vec3 viewPosition;
 
-out vec4 outputColor;
+smooth out vec4 interpColor;
+
+// fixed point light properties
+vec3 light_position_world  = vec3 (10.0, 10.0, 10.0);
+vec3 Ls = vec3 (1.0, 1.0, 1.0); // white specular colour
+vec3 Ld = vec3 (0.7, 0.7, 0.7); // dull white diffuse light colour
+vec3 La = vec3 (0.2, 0.2, 0.2); // grey ambient colour
+  
+// surface reflectance
+vec3 Ks = vec3 (1.0, 1.0, 1.0); // fully reflect specular light
+vec3 Kd = vec3 (1.0, 0.5, 0.0); // orange diffuse surface reflectance
+vec3 Ka = vec3 (1.0, 1.0, 1.0); // fully reflect ambient light
+float specular_exponent = 100.0; // specular 'power'
 
 void main()
 {
 	interpColor = interpColor + vec4(La * Ka, 1.0);
 
-	lightDirection = normalize(light_position_world - vec3(vertexPosition));
-	cosAngIncidence = dot(normalDirection, lightDirection);
+	vec3 lightDirection = normalize(light_position_world - vec3(vertexPosition));
+	vec3 viewDirection = normalize(viewPosition);
+	cosAngIncidence = dot(normalize(normalDirection), lightDirection);
 	cosAngIncidence = clamp(cosAngIncidence, 0, 1);
 
 	interpColor = interpColor + vec4(Ld * Kd * cosAngIncidence, 1.0);
 
-	reflectDirection = reflect(-lightDirection, normalDirection);
+	reflectDirection = reflect(-lightDirection, normalize(normalDirection));
 	phongTerm = dot(viewDirection, reflectDirection);
 	phongTerm = clamp(phongTerm, 0, 1);
 
@@ -25,6 +38,4 @@ void main()
 	phongTerm = pow(phongTerm, specular_exponent);
 
 	interpColor = interpColor + vec4(Ls * Ks * phongTerm, 1.0);
-
-    	outputColor = interpColor;
 }
--- vertexshader_phong.vs
+++ vertexshader_phong.vs
@@ -22,25 +22,13 @@ uniform mat4 ModelMatrix;
 layout (location = 0) in vec3 Position;
 layout (location = 1) in vec3 Normal;
 
-out vec3 normalPosition;
+out vec3 normalDirection;
 out vec3 viewPosition;
-
-// fixed point light properties
-vec3 light_position_world  = vec3 (10.0, 10.0, 10.0);
-vec3 Ls = vec3 (1.0, 1.0, 1.0); // white specular colour
-vec3 Ld = vec3 (0.7, 0.7, 0.7); // dull white diffuse light colour
-vec3 La = vec3 (0.2, 0.2, 0.2); // grey ambient colour
-  
-// surface reflectance
-vec3 Ks = vec3 (1.0, 1.0, 1.0); // fully reflect specular light
-vec3 Kd = vec3 (1.0, 0.5, 0.0); // orange diffuse surface reflectance
-vec3 Ka = vec3 (1.0, 1.0, 1.0); // fully reflect ambient light
-float specular_exponent = 100.0; // specular 'power'
 
 void main()
 {
 	vec4 vertexPosition = ModelMatrix * vec4(Position, 1.0);
 	gl_Position = ProjectionMatrix*ViewMatrix*vertexPosition;
-	vec3 normalPosition = vec3(ModelMatrix * vec4(Normal, 0.0));
+	vec3 normalDirection = normalize(vec3(ModelMatrix * vec4(Normal, 0.0)));
 	vec3 viewPosition = vec3(inverse(ViewMatrix) * vec4(0.0, 0.0, 0.0, 1.0) - vertexPosition);
 }
